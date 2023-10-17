601a51daee12754119c6078f7501d7675c25287e
Harrison.Blair <harrison.blair@mymail.champlain.edu>
2023-10-16 21:24:27 -0400

    week 8 intro to powershell assignment

---
+++ Week-8/logSearch.ps1
@@ -0,0 +1,10 @@
+﻿
+$myDir = "C:\Users\champuser\Desktop"
+
+Get-EventLog -list
+
+$readLog = Read-host -Prompt "Please select a log to review from the above list"
+
+$searchTerm = Read-host -Prompt "Please indicate an item you wish to search for"
+
+Get-EventLog -LogName $readLog -Newest 40 | where {$_.Message -ilike "*$searchTerm*"} | export-csv -NoTypeInformation -Path "$myDir\securitylogs.csv"
