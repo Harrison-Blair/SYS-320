6ed17218857fdae07382c50bdcb0ae3f5d51b6c0
Harrison.Blair <harrison.blair@mymail.champlain.edu>
2023-11-29 20:18:27 -0500

    Week-12 Tutorial Followed

---
+++ Week-12/SSH.ps1
@@ -0,0 +1,22 @@
+﻿cls
+$the_ip = read-host -Prompt "Please enter the IP for the SSH server : "
+
+# Login to SSH Server
+New-SSHSession -ComputerName $the_ip -Credential (Get-Credential sys320)
+
+While ($True) {
+    #Prompt for user
+    $the_cmd = read-host -Prompt "Please enter a command : "
+
+    #Run Command on remote SSH Server
+    (Invoke-SSHCommand -index 0 $the_cmd).Output
+}
+
+cls
+
+echo "Removing Session..."
+sleep 2
+Remove-SSHSession -SessionId 0
+echo "Session Removed."
+
+
